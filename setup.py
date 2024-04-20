13197aa9a4aa51febe45e0527e25bb7948d4d2a6
kimjinsu <tjtfy7028@gmail.com>
2024-04-21 02:29:21 +0900

    d

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='simple_calculator',
+    version='0.0.1',
+    description='simple calculator',
+    author='jins',
+    url='https://github.com/plan11plan/simple',
+    download_url='https://github.com/plan11plan/simple',
+    packages=['test'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+
+)
